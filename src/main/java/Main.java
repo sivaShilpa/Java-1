2164c317e23e02138588c5eb4bba461df92ef51b
sivaShilpa <shilpa.vchand@gmail.com>
2022-08-22 16:00:17 -0400

    2/10

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -2,7 +2,23 @@ import java.util.Scanner;
 
 public class Main {
     public static void main(String[] args) {
-        ex1();
+//        ex1();
+        ex2();
+    }
+
+    private static void ex2() {
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("Enter a string: ");
+        int upper = 0;
+        String string = scanner.nextLine();
+
+        for(int i = 0 ; i < string.length(); i++){
+            char ch = string.charAt(i);
+            if (ch >= 'A' && ch <= 'Z')
+                upper++;
+        }
+
+        System.out.println("Number of uppercase letters: " + upper);
     }
 
     private static void ex1() {
