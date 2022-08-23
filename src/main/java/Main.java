0dd5f5d982e3049696a0e6a26d29d683e2e52f43
sivaShilpa <shilpa.vchand@gmail.com>
2022-08-23 13:53:13 -0400

    3/10

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,9 +1,34 @@
 import java.util.Scanner;
+import java.util.StringTokenizer;
 
 public class Main {
     public static void main(String[] args) {
 //        ex1();
-        ex2();
+//        ex2();
+        ex3();
+    }
+
+    private static void ex3() {
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("Enter a sentence: ");
+        String input = scanner.nextLine();
+        int wordCounter = 0;
+        String token = "";
+        String output = "";
+
+        StringTokenizer stringTokenizer = new StringTokenizer(input);
+
+        while(stringTokenizer.hasMoreTokens()){
+            token = stringTokenizer.nextToken();
+            wordCounter++;
+
+            if(wordCounter % 2 != 0){
+                token = token.toUpperCase();
+            }
+            output += token + " ";
+        }
+
+        System.out.println(output);
     }
 
     private static void ex2() {
